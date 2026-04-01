b043d5fda730ac76eee8f867718c0bb18e9b21ad
lorebianco <115111447+lorebianco@users.noreply.github.com>
2026-04-01 15:11:57 +0000

    Deploying to gh-pages from @ lorebianco/muEDM_Analysis@1a1861a144acb0580cdb64a5431b39464900c6d9 🚀

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['setactivecylinders_0',['SetActiveCylinders',['../namespaceCHeT_1_1Config.html#a85c38200ec4f7c7767b7113c488d240e',1,'CHeT::Config']]],
-  ['setcuts_1',['SetCuts',['../classCHeT_1_1Data_1_1Reader.html#ae481cb0121a10c46fee38835fb161ff2',1,'CHeT::Data::Reader']]],
-  ['setdelta1_2',['SetDelta1',['../namespaceCHeT_1_1Config.html#a707e776a19efa42bedee0f8187a9ec87',1,'CHeT::Config']]],
-  ['setdelta2_3',['SetDelta2',['../namespaceCHeT_1_1Config.html#a73e7e155e52d8d5f219c67c62e90f2da',1,'CHeT::Config']]],
-  ['setenabledboards_4',['SetEnabledBoards',['../classCHeT_1_1Data_1_1Reader.html#ad0a1fe28e0d3fc9db2738d224a701f4d',1,'CHeT::Data::Reader']]],
-  ['setenabledcylinders_5',['SetEnabledCylinders',['../classCHeT_1_1Data_1_1Reader.html#a03f72c2389472b342a92c88a87090d12',1,'CHeT::Data::Reader']]],
-  ['setenabledgeometries_6',['SetEnabledGeometries',['../classCHeT_1_1Data_1_1Reader.html#ac68039b496cf0f331ec74a64906bd8c4',1,'CHeT::Data::Reader']]],
-  ['setenabledlayers_7',['SetEnabledLayers',['../classCHeT_1_1Data_1_1Reader.html#a23764da54d0c4fe9dd4c6accd45cd410',1,'CHeT::Data::Reader']]],
-  ['setoffsetexp_8',['SetOffsetExp',['../namespaceCHeT_1_1Config.html#a8fb9d954407ebe6f76199b2f2db0d003',1,'CHeT::Config']]],
-  ['setrotation_9',['SetRotation',['../namespaceCHeT_1_1Config.html#ae0d17c6e8eb64bfc9ab9baf1054d5dbd',1,'CHeT::Config']]],
-  ['setsingleevent_10',['SetSingleEvent',['../classCHeT_1_1Data_1_1Reader.html#a1d4a0fcee9a8ff5c640763263d5f76c2',1,'CHeT::Data::Reader']]],
-  ['settranslation_11',['SetTranslation',['../namespaceCHeT_1_1Config.html#aad1a61c30c5444ff3b07b73111a8474e',1,'CHeT::Config']]]
+  ['savetotree_0',['SaveToTree',['../classCHeT_1_1Data_1_1Reader.html#aa97c890989b02515ef179e654f91991d',1,'CHeT::Data::Reader']]],
+  ['setactivecylinders_1',['SetActiveCylinders',['../namespaceCHeT_1_1Config.html#a85c38200ec4f7c7767b7113c488d240e',1,'CHeT::Config']]],
+  ['setcuts_2',['SetCuts',['../classCHeT_1_1Data_1_1Reader.html#ae481cb0121a10c46fee38835fb161ff2',1,'CHeT::Data::Reader']]],
+  ['setdelta1_3',['SetDelta1',['../namespaceCHeT_1_1Config.html#a707e776a19efa42bedee0f8187a9ec87',1,'CHeT::Config']]],
+  ['setdelta2_4',['SetDelta2',['../namespaceCHeT_1_1Config.html#a73e7e155e52d8d5f219c67c62e90f2da',1,'CHeT::Config']]],
+  ['setenabledboards_5',['SetEnabledBoards',['../classCHeT_1_1Data_1_1Reader.html#ad0a1fe28e0d3fc9db2738d224a701f4d',1,'CHeT::Data::Reader']]],
+  ['setenabledcylinders_6',['SetEnabledCylinders',['../classCHeT_1_1Data_1_1Reader.html#a03f72c2389472b342a92c88a87090d12',1,'CHeT::Data::Reader']]],
+  ['setenabledgeometries_7',['SetEnabledGeometries',['../classCHeT_1_1Data_1_1Reader.html#ac68039b496cf0f331ec74a64906bd8c4',1,'CHeT::Data::Reader']]],
+  ['setenabledlayers_8',['SetEnabledLayers',['../classCHeT_1_1Data_1_1Reader.html#a23764da54d0c4fe9dd4c6accd45cd410',1,'CHeT::Data::Reader']]],
+  ['setoffsetexp_9',['SetOffsetExp',['../namespaceCHeT_1_1Config.html#a8fb9d954407ebe6f76199b2f2db0d003',1,'CHeT::Config']]],
+  ['setrotation_10',['SetRotation',['../namespaceCHeT_1_1Config.html#ae0d17c6e8eb64bfc9ab9baf1054d5dbd',1,'CHeT::Config']]],
+  ['setsingleevent_11',['SetSingleEvent',['../classCHeT_1_1Data_1_1Reader.html#a1d4a0fcee9a8ff5c640763263d5f76c2',1,'CHeT::Data::Reader']]],
+  ['settranslation_12',['SetTranslation',['../namespaceCHeT_1_1Config.html#aad1a61c30c5444ff3b07b73111a8474e',1,'CHeT::Config']]]
 ];
