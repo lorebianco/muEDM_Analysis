2a8b047ba7dee0414e11251f12ea7befc70e5daf
lorebianco <115111447+lorebianco@users.noreply.github.com>
2026-05-07 10:10:08 +0000

    Deploying to gh-pages from @ lorebianco/muEDM_Analysis@08898379c24ac1d1993076e7685389d3993f89b3 🚀

--- search/all_12.js
+++ search/all_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['t_5fmax_0',['t_max',['../structCHeT_1_1Vis_1_1VisHelixTrack.html#ae2c21adb2c31f04048e7605a0cfb3fc6',1,'CHeT::Vis::VisHelixTrack']]],
-  ['t_5fmin_1',['t_min',['../structCHeT_1_1Vis_1_1VisHelixTrack.html#a1c9703f71c4a59cfbc1212c3024747be',1,'CHeT::Vis::VisHelixTrack']]]
+  ['t_5fmin_1',['t_min',['../structCHeT_1_1Vis_1_1VisHelixTrack.html#a1c9703f71c4a59cfbc1212c3024747be',1,'CHeT::Vis::VisHelixTrack']]],
+  ['thickness_2',['thickness',['../structCHeT_1_1Config_1_1LayerConfig.html#a1543b70d81054876801c82cee60596fc',1,'CHeT::Config::LayerConfig']]]
 ];
--- search/all_c.js
+++ search/all_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['nbundles_0',['nBundles',['../structCHeT_1_1Config_1_1LayerConfig.html#a4d2621de6262af7bf14d0bc32100861d',1,'CHeT::Config::LayerConfig']]],
-  ['nominalstereoangle_1',['nominalStereoAngle',['../structCHeT_1_1Config_1_1LayerConfig.html#a86e1106e9cac69462db539a1d604789e',1,'CHeT::Config::LayerConfig']]]
+  ['nominalradius_1',['nominalRadius',['../structCHeT_1_1Config_1_1CylinderConfig.html#aca0765e303c6dd3b448324bfa9ae4e8c',1,'CHeT::Config::CylinderConfig']]],
+  ['nominalstereoangle_2',['nominalStereoAngle',['../structCHeT_1_1Config_1_1LayerConfig.html#a86e1106e9cac69462db539a1d604789e',1,'CHeT::Config::LayerConfig']]]
 ];
